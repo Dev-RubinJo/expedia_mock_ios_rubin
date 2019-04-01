d93090c4b42116a096ae5ee29fd59f6f6dce9e78
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-01 23:23:30 +0900

    update UI

--- Expedia/TripReservartion/TripReservationViewController.swift
+++ Expedia/TripReservartion/TripReservationViewController.swift
@@ -11,19 +11,28 @@ import UIKit
 class TripReservationViewController: UIViewController {
 
     @IBOutlet weak var topLogoView: UIView!
+    @IBOutlet weak var reservateHotelButton: UIButton!
+    
     
     
     func updateUI() {
-        self.topLogoView.layer.borderColor = UIColor.lightGray.cgColor
-        self.topLogoView.layer.borderWidth = 1.0
+        self.topLogoView.layer.addBorder([.bottom], color: UIColor.lightGray, width: 1.0)
+        self.reservateHotelButton.layer.addBorder([.bottom], color: UIColorFromRGB(rgbValue: 0xFFCB06), width: 3.0)
+    }
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.updateUI()
         // Do any additional setup after loading the view.
     }
 
 
 }
-
