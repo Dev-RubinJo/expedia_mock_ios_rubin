c3e8c6edd569bf51081a3145b0259ab0dfc54953
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-01 23:22:59 +0900

    update ScheduleViewController

---
+++ Expedia/Schedule/ScheduleViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ScheduleViewController.swift
+//  Expedia
+//
+//  Created by YooBin Jo on 01/04/2019.
+//  Copyright © 2019 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    
+    @IBOutlet weak var topLabelView: UIView!
+    
+    func updateUI() {
+        self.topLabelView.layer.addBorder([.bottom], color: UIColor.lightGray, width: 1.0)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.updateUI()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor
+            self.addSublayer(border)
+        }
+    }
+}
