6bffedb8b1f88417009216d57b287ded5a3e956a
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-02 20:25:53 +0900

    오늘의 인기 특가 셀 생성

---
+++ Expedia/TripReservartion/TRCustomCell/TodayPriceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodayPriceTableViewCell.swift
+//  Expedia
+//
+//  Created by YooBin Jo on 02/04/2019.
+//  Copyright © 2019 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+class TodayPriceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
