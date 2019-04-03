4e5e3c1bdda406622a667feb445578256803a803
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-03 17:11:08 +0900

    마감 임박 특가 수정

--- Expedia/TripReservartion/TRCustomCell/LimitedDatePriceTableViewCell.swift
+++ Expedia/TripReservartion/TRCustomCell/LimitedDatePriceTableViewCell.swift
@@ -11,9 +11,11 @@ import UIKit
 class LimitedDatePriceTableViewCell: UITableViewCell {
 
     @IBOutlet weak var limitedDatePriceButton: UIButton!
+    @IBOutlet weak var limitedPriceIcon: UIImageView!
     
     func updateUI() {
         limitedDatePriceButton.layer.cornerRadius = 10
         limitedDatePriceButton.layer.masksToBounds = true
+        limitedPriceIcon.layer.cornerRadius = limitedPriceIcon.frame.width / 2
     }
 }
