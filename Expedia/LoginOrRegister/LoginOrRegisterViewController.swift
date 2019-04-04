065a043f6b4f21d197edf722ba5b89b121854656
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-04 13:43:48 +0900

    로그인, 회원가입 뷰 계속 수정중

--- Expedia/LoginOrRegister/LoginOrRegisterViewController.swift
+++ Expedia/LoginOrRegister/LoginOrRegisterViewController.swift
@@ -14,6 +14,34 @@ class LoginOrRegisterViewController: ButtonBarPagerTabStripViewController {
     @IBAction func closeButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    func configureButtonBar() {
+        // Sets the background colour of the pager strip and the pager strip item
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        
+        // Sets the pager strip item font and font color
+        settings.style.buttonBarItemFont = UIFont(name: "Helvetica", size: 16.0)!
+        settings.style.buttonBarItemTitleColor = .gray
+        
+        // Sets the pager strip item offsets
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemsShouldFillAvailableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        
+        // Sets the height and colour of the slider bar of the selected pager tab
+        settings.style.selectedBarHeight = 3.0
+        settings.style.selectedBarBackgroundColor = .orange
+        
+        // Changing item text color on swipe
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .gray
+            newCell?.label.textColor = .orange
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,8 +54,6 @@ class LoginOrRegisterViewController: ButtonBarPagerTabStripViewController {
         child2.childNumber = "Two"
         return [child1, child2]
     }
-    
-    
 
 }
 //extension LoginOrRegisterViewController: Pager
