52750ae700e9b59e216d3e711c1109fc7b0d298e
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-04 11:47:37 +0900

    마지막 탭(계정) 뷰컨트롤러 생성

---
+++ Expedia/New Group1/AccountsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountsViewController.swift
+//  Expedia
+//
+//  Created by YooBin Jo on 04/04/2019.
+//  Copyright © 2019 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+class AccountsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
