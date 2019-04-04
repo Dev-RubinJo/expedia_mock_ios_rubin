13e7517c2da2f43429594dff3c2ef16e43119560
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-05 01:14:14 +0900

    로그인 뷰 화면 수정중

--- Expedia/LoginOrRegister/LoginViewController.swift
+++ Expedia/LoginOrRegister/LoginViewController.swift
@@ -8,9 +8,35 @@
 
 import UIKit
 import XLPagerTabStrip
+import TextFieldEffects
 
 class LoginViewController: UIViewController, IndicatorInfoProvider {
-
+    
+    let emailTextField = HoshiTextField(frame: CGRect(x: 10.0, y: 145.0, width: UIScreen.main.bounds.width - 20, height: 45.0))
+    let passwardTextField = HoshiTextField(frame: CGRect(x: 10.0, y: 210.0, width: UIScreen.main.bounds.width - 20, height: 45))
+    
+    let forgetPassward = UIButton(frame: CGRect(x: 90.0, y: 285.0, width: 180, height: 30))
+    
+    
+    func updateUI() {
+        self.view.addSubview(emailTextField)
+        self.view.addSubview(passwardTextField)
+        self.view.addSubview(forgetPassward)
+        
+        emailTextField.placeholder = " 이메일 주소"
+        emailTextField.placeholderFontScale = 0.77
+        emailTextField.borderStyle = .roundedRect
+        emailTextField.placeholderColor = .darkGray
+        
+        passwardTextField.placeholder = " 비밀번호"
+        passwardTextField.placeholderFontScale = 0.77
+        passwardTextField.borderStyle = .roundedRect
+        passwardTextField.placeholderColor = .darkGray
+        
+        forgetPassward.setTitle("비밀번호를 잊으셨나요?", for: UIControl.State.normal)
+        forgetPassward.setTitleColor(.blue, for: UIControl.State.normal)
+        
+    }
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "로그인")
@@ -18,6 +44,8 @@ class LoginViewController: UIViewController, IndicatorInfoProvider {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
 
         // Do any additional setup after loading the view.
     }
