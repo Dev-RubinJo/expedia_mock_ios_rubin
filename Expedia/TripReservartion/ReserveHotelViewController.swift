da0e1d297be75eb0147938c731c1573b22a5021a
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-05 17:12:57 +0900

    호텔 예약하기 viewController생성중

--- Expedia/TripReservartion/ReserveHotelViewController.swift
+++ Expedia/TripReservartion/ReserveHotelViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ReserveHotelViewController: UIViewController {
-
+    
+    var inputDestinationButtonText: String = "목적지 입력"
+    var destination: String?
+    
+    
+    @IBOutlet weak var inputDestinationButton: UIButton!
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func updateUI() {
+        self.navigationItem.title = "호텔 예약하기"
+        let inputDestinationButtonFont: UIFont! = UIFont(name: "HelveticaNeue", size: 17.0)
+        inputDestinationButton.setTitle("\(inputDestinationButtonText)", for: UIControl.State.normal)
+        inputDestinationButton.titleLabel?.font = inputDestinationButtonFont
+        inputDestinationButton.setTitleColor(.lightGray, for: UIControl.State.normal)
+        inputDestinationButton.layer.borderColor = UIColor.lightGray.cgColor
+        inputDestinationButton.layer.borderWidth = 1.0
+        inputDestinationButton.layer.cornerRadius = 10.0
+        inputDestinationButton.layer.masksToBounds = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
 
