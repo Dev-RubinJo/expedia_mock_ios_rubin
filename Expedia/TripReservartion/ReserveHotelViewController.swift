a61c5dbe17d13cc167e632b955292e800bbbc470
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-05 15:00:47 +0900

    호텔 예약하기 viewController 생성

---
+++ Expedia/TripReservartion/ReserveHotelViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReserveHotelViewController.swift
+//  Expedia
+//
+//  Created by YooBin Jo on 05/04/2019.
+//  Copyright © 2019 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+class ReserveHotelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
