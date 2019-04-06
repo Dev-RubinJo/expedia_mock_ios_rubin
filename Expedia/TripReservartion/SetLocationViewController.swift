3eb0bf823e921857e402f06cf9fe19c7b84ddca5
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-06 12:54:37 +0900

    호텔 위치 지정하는 뷰 생성

---
+++ Expedia/TripReservartion/SetLocationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SetLocationViewController.swift
+//  Expedia
+//
+//  Created by YooBin Jo on 06/04/2019.
+//  Copyright © 2019 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class SetLocationViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+
+    
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func getLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
