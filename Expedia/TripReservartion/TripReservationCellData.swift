e0ee15175f0b86dde6ed8e3f2377ce23f8bc3cd8
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-06 21:12:07 +0900

    셀 데이터 클래스 생성

---
+++ Expedia/TripReservartion/TripReservationCellData.swift
@@ -0,0 +1,21 @@
+//
+//  TripReservationCellData.swift
+//  Expedia
+//
+//  Created by YooBin Jo on 06/04/2019.
+//  Copyright © 2019 YooBin Jo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class TripReservationCellData {
+    
+    let day: UIImage! = UIImage(named: "day")
+    let eight: UIImage! = UIImage(named: "eight")
+    
+    let cellTitle: Array<String> = ["₩80,000 이하 특가 호텔", "일일 특가"]
+    let cellContents: Array<String> = ["호텔이 1박에 ₩80,000 이하!", "매일 일부 호텔을 40% 이상 할인해 드려요. 매일 밤 자정 특가 상품이 업데이트 됩니다."]
+    
+}
