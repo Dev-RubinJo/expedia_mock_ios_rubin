710138e4366dad76a3f4c4d5b0769c47501edc69
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-07 19:57:44 +0900

    8만원이하 호텔 목록 나오게 하는 뷰 생성

---
+++ Expedia/TripReservartion/EightPriceHotelList/EightPriceHotelViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EightPriceHotelViewController.swift
+//  Expedia
+//
+//  Created by YooBin Jo on 07/04/2019.
+//  Copyright © 2019 YooBin Jo. All rights reserved.
+//
+
+import UIKit
+
+
+class EightPriceHotelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
