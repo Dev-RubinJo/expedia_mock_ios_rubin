d114edfacd97c50b7596ba397ab19af3dd60049c
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-07 22:25:32 +0900

    현재 SnapKit을 이용한 뷰 만들기. 하드코딩중

--- Expedia/Accounts/AccountsViewController.swift
+++ Expedia/Accounts/AccountsViewController.swift
@@ -13,11 +13,30 @@ class AccountsViewController: UIViewController {
     
     
     let data = AccountsData()
+    var isLogin = false
     
-    let topView = UIView()
-    let topViewLabel = UILabel()
+    let topView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    //(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+    let topViewLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "계정"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.center
+        return label
+    }()
     
-    let mainScrollView = UIScrollView()
+    let mainScrollView: UIScrollView = {
+        let color = UIColor()
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = color.UIColorFromRGB(rgbValue: 0xF1EFF6)
+        return scrollView
+    }()
     
     let loginButton: UIButton = {
         let color = UIColor()
@@ -27,28 +46,144 @@ class AccountsViewController: UIViewController {
         button.setTitle("익스피디아에 로그인", for: UIControl.State.normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: UIFont.Weight.regular)
         button.setImage(UIImage(named: "LoginToExpedia"), for: UIControl.State.normal)
+        button.layer.cornerRadius = 10.0
         return button
     }()
     
-    let accountLabel = UILabel()
-    let currencyCell = UIView()
+    let accountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "계정"
+        label.font = UIFont.systemFont(ofSize: 17, weight: .light)
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.left
+        return label
+    }()
+    let currencyCell: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    let currencyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "통화"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .ultraLight)
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.left
+        return label
+    }()
+    let currencyUnitLabel: UILabel = {
+        let label = UILabel()
+        label.text = "KRW"
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.left
+        return label
+    }()
+    let countryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "국가"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .ultraLight)
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.left
+        return label
+    }()
+    let chooseCountryButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
     
-    //    @IBOutlet weak var accountTableView: UITableView!
-    
-    func updateUI() {
+    func updateUI(isLogin: Bool) {
+        
+        self.view.addSubview(topView)
+        self.topView.addSubview(topViewLabel)
+        self.view.addSubview(mainScrollView)
+        
+        topView.snp.makeConstraints { make in
+            make.top.equalTo(37)
+            make.leading.equalTo(0)
+            make.trailing.equalTo(0)
+            make.height.equalTo(50)
+        }
+        topViewLabel.snp.makeConstraints{ make in
+            make.top.equalTo(topView.snp_top).offset(5)
+            make.leading.equalTo(topView.snp_leading).offset(10)
+            make.trailing.equalTo(topView.snp_trailing).offset(-10)
+            make.bottom.equalTo(topView.snp_bottom).offset(5)
+        }
+        
+        mainScrollView.snp.makeConstraints { make in
+            make.top.equalTo(topView.snp.bottom).offset(0)
+            make.height.equalTo(1140)
+            make.width.equalTo(UIScreen.main.bounds.width)
+        }
+        if isLogin == false {
+            self.mainScrollView.addSubview(loginButton)
+            loginButton.snp.makeConstraints { make in
+                make.top.equalTo(15)
+                make.leading.equalTo(10)
+                make.trailing.equalTo(10)
+                make.width.equalTo(UIScreen.main.bounds.width - 20)
+                make.height.equalTo(60)
+            }
+            self.mainScrollView.addSubview(accountLabel)
+            accountLabel.snp.makeConstraints { make in
+                make.top.equalTo(loginButton.snp.bottom).offset(10)
+                make.leading.equalTo(10)
+                make.height.equalTo(40)
+            }
+            self.mainScrollView.addSubview(currencyCell)
+            currencyCell.snp.makeConstraints { make in
+                make.top.equalTo(accountLabel.snp.bottom).offset(1)
+                make.leading.equalTo(0)
+                make.trailing.equalTo(0)
+                make.height.equalTo(60)
+            }
+            self.currencyCell.addSubview(currencyLabel)
+            currencyLabel.snp.makeConstraints { make in
+                make.top.equalTo(10)
+                make.leading.equalTo(10)
+            }
+            self.currencyCell.addSubview(currencyUnitLabel)
+            currencyUnitLabel.snp.makeConstraints { make in
+                make.top.equalTo(currencyLabel.snp.bottom).offset(5)
+                make.leading.equalTo(10)
+            }
+        } else {
+            self.mainScrollView.addSubview(accountLabel)
+            accountLabel.snp.makeConstraints { make in
+                make.top.equalTo(mainScrollView.snp.top).offset(10)
+                make.leading.equalTo(10)
+                make.height.equalTo(40)
+            }
+            self.mainScrollView.addSubview(currencyCell)
+            currencyCell.snp.makeConstraints { make in
+                make.top.equalTo(accountLabel.snp.bottom).offset(1)
+                make.leading.equalTo(0)
+                make.trailing.equalTo(0)
+                make.height.equalTo(60)
+            }
+            self.currencyCell.addSubview(currencyLabel)
+            currencyLabel.snp.makeConstraints { make in
+                make.top.equalTo(10)
+                make.leading.equalTo(10)
+            }
+            self.currencyCell.addSubview(currencyUnitLabel)
+            currencyUnitLabel.snp.makeConstraints { make in
+                make.top.equalTo(currencyLabel.snp.bottom).offset(5)
+                make.leading.equalTo(10)
+            }
+            
+        }
+        
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(loginButton)
+        updateUI(isLogin: self.isLogin)
         
-        loginButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.width.equalTo(UIScreen.main.bounds.width - 20)
-            make.height.equalTo(60)
-        }
+        
 //        accountTableView.delegate = self
 //        accountTableView.dataSource = self
         
