334f620ae7477e355ace54de0dddab8630e26046
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-10 21:45:04 +0900

    호텔 리스트 셀 UI 구성요소 변경

--- Expedia/TripReservartion/HotelListTableViewCell.swift
+++ Expedia/TripReservartion/HotelListTableViewCell.swift
@@ -14,8 +14,10 @@ class HotelListTableViewCell: UITableViewCell {
     var imageURL: URL?
     let color = UIColor()
 
+    @IBOutlet weak var baseView: UIView!
     @IBOutlet weak var hotelImg: UIImageView!
     @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var percentView: UIView!
     @IBOutlet weak var hotelNameLabel: UILabel!
     @IBOutlet weak var hotelLocationLabel: UILabel!
     @IBOutlet weak var hotelPriceLabel: UILabel!
@@ -24,15 +26,26 @@ class HotelListTableViewCell: UITableViewCell {
         
         self.backgroundColor = color.UIColorFromRGB(rgbValue: 0xF1EFF6)
         
+        self.baseView.layer.cornerRadius = 5
+        self.baseView.layer.masksToBounds = true
+        
+//        self.hotelImg.layer.cornerRadius = 5
+        
+        
         let percentText = info[indexPath]["Percentage"].intValue
         let hotelNameText = info[indexPath]["Name"].stringValue
         let hotelLocationText = info[indexPath]["ShortL"].stringValue
         let hotelPriceText = info[indexPath]["Priced"].stringValue
         
+        self.percentView.backgroundColor = UIColor.green
+        self.percentView.layer.cornerRadius = 10
+        
         self.percentLabel.text = "\(percentText)%"
         self.hotelNameLabel.text = hotelNameText
         self.hotelLocationLabel.text = hotelLocationText
         self.hotelPriceLabel.text = hotelPriceText
+        
+        
     }
     
 }
