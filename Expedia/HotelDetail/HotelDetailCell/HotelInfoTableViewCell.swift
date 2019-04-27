04c14ff55db7a155ae0f763d2f8e191047dc409d
YooBin Jo <dbqlsdl1234@naver.com>
2019-04-27 16:34:26 +0900

    호텔 상세정보 셀 수정

--- Expedia/HotelDetail/HotelDetailCell/HotelInfoTableViewCell.swift
+++ Expedia/HotelDetail/HotelDetailCell/HotelInfoTableViewCell.swift
@@ -12,78 +12,66 @@ import SwiftyJSON
 
 class HotelInfoTableViewCell: UITableViewCell, UIScrollViewDelegate {
 
-    
-    var hotelImageNum: Int?
-    var info: JSON?
-    
+    private let color = UIColor()
     
     @IBOutlet weak var hotelImgViewPager: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var priceView: UIView!
     @IBOutlet weak var priceLabel: UILabel!
-    @IBOutlet weak var infoDetailLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var mapButton: UIButton!
+    @IBOutlet weak var hotelNameLabel: UILabel!
+    @IBOutlet weak var startDateTextField: UITextField!
+    @IBOutlet weak var endDateTextField: UITextField!
     
     
-    func updateUI() {
-        self.hotelImageNum = 5
-        for index in 0 ..< hotelImageNum! {
-            let subView = UIView()
+    
+    func updateUI(_ data: JSON, _ imageList: [JSON], _ hotelPrice: String) {
+        for index in 0 ..< imageList.count {
+            let hotelImage = imageList[index].stringValue
+            let hotelImageURL = URL(string: hotelImage)
+            let imgData = NSData(contentsOf: hotelImageURL!)
+            let image = UIImage(data: imgData! as Data)
+            let subView = UIImageView(image: image!)
             subView.frame = hotelImgViewPager.frame
-            subView.backgroundColor = UIColor(
-                red: CGFloat(index * 50) / 255.0,
-                green: CGFloat(index * 50) / 255.0,
-                blue: CGFloat(index * 50) / 255.0,
-                alpha: 1
-            )
             subView.frame.origin.x = UIScreen.main.bounds.width * CGFloat(index)
             hotelImgViewPager.addSubview(subView)
         }
         hotelImgViewPager.isPagingEnabled = true
         hotelImgViewPager.contentSize = CGSize(
-            width: UIScreen.main.bounds.width * CGFloat(hotelImageNum!),
+            width: UIScreen.main.bounds.width * CGFloat(imageList.count),
             height: hotelImgViewPager.frame.height
         )
         
         hotelImgViewPager.alwaysBounceVertical = false
         
-        pageControl.numberOfPages = hotelImageNum!
+        pageControl.numberOfPages = imageList.count
         // // // // // // // //
-        priceView.layer.addBorder([.bottom], color: UIColor.lightGray, width: 1.0)
+        priceView.layer.addBorder([.bottom], color: self.color.UIColorFromRGB(rgbValue: 0xF6F6F7), width: 1.0)
         
-        
-        let location = CLLocationCoordinate2D(latitude: 37.77, longitude: 136.6)
+        //////
+        let location = CLLocationCoordinate2D(latitude: CLLocationDegrees(data[data.count - 1]["lat"].floatValue), longitude: CLLocationDegrees(data[data.count - 1]["lng"].floatValue))
         let region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
         mapView.setRegion(region, animated: true)
         
-        let pin = CustomPin(pinTitle: "서울", pinSubTitle: "한국, 서울", location: location)
+        let pin = CustomPin(pinTitle: "\(data[data.count - 1]["Name"].stringValue)", pinSubTitle: "\(data[data.count - 1]["Location"].stringValue)", location: location)
         mapView.addAnnotation(pin)
         
         
+        //////////////////////
+        self.hotelNameLabel.text = data[data.count - 1]["Name"].stringValue
+        self.priceLabel.text = "₩\(hotelPrice)"
         
         
+        //////////////////////
+        
+        self.startDateTextField.text = data[data.count - 1]["Sdate"].stringValue
+        self.endDateTextField.text = data[data.count - 1]["Edate"].stringValue
+
     }
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         pageControl.currentPage = Int(floor(hotelImgViewPager.contentOffset.x / hotelImgViewPager.frame.width))
     }
-    
-    
-    
-    
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
 class CustomPin: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
